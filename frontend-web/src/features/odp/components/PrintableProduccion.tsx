4a2a3be0122b6c0290f0e5e8a1868e942e654aab
Test User <test@example.com>
2026-03-13 18:01:26 -0500

    fix: habilitar vista previa en pantalla para formato OP

--- frontend-web/src/features/odp/components/PrintableProduccion.tsx
+++ frontend-web/src/features/odp/components/PrintableProduccion.tsx
@@ -9,7 +9,7 @@ interface PrintableProduccionProps {
 const PrintableProduccion: React.FC<PrintableProduccionProps> = ({ odp }) => {
 
     return (
-        <div className="hidden print:block w-[21.5cm] min-h-[29cm] bg-white text-black font-sans text-[10px] mx-auto overflow-hidden">
+        <div className="block print:block shadow-xl print:shadow-none w-[21.5cm] min-h-[29cm] bg-white text-black font-sans text-[10px] mx-auto overflow-hidden">
             <style>
                 {`
                 .excel-table { width: 100%; border-collapse: collapse; border: 2px solid #000; }
